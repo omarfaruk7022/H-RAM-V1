fd8edc306f382704ae572a1e02a22e4008b1ec4f
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-05-10 17:51:28 +0600

    data post and get succes

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId} = require("mongodb");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.78edo5s.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.78edo5s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to database");
+    const usersCollection = client.db("H-RAM-V1").collection("users");
+
+    // GET API
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        res.json(result);
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
